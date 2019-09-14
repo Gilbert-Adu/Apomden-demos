93607bddb959baca8cf35dc6b1b3a7a22db380e0
Gilbert Adu <gea33@cornell.edu>
2019-09-14 06:18:12 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    <div className="thetext">
+      Choose your operating system:
+    </div>
+
+    <div className="thebuttons">
+
+
+
+      <button><div className="info"><a href="apomden.py" download>Mac OS X</a></div></button>
+      <button><div className="info"><a href="apomden.py" download>Windows 10+</a></div></button>
+      <button><div className="info"><a href="apomden.py" download>Linux</a></div></button>
+
+    </div>
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
